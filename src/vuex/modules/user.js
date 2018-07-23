70cad9189f572383c3a3475bd7d5bedbe3533aff
chenshixiong <1067816765@qq.com>
2018-07-23 16:57:33 +0800

    使用less

--- src/vuex/modules/user.js
+++ src/vuex/modules/user.js
@@ -14,14 +14,17 @@ const user = {
         // 登录
         Login({ commit }, userInfo) {
             return new Promise((resolve, reject) => {
-                login(userInfo).then(response => {
+                /* login(userInfo).then(response => { // 登录到泉州食达康
                     console.log(response)
                     setToken(response.data.token)
                     commit('SET_TOKEN', response.data.token)
                     resolve()
                 }).catch(error => {
                     reject(error)
-                })
+                })*/
+              setToken('alkdadakjhkqyq3yiehsdkj') // 模拟直接登入
+              commit('SET_TOKEN', 'alkdadakjhkqyq3yiehsdkj')
+              resolve()
             })
         }
     }
